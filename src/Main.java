9c1425a397e2dc9f2e60f75b3c2f53f47ed5179d
Alisson2003 <alisson.viracocha@epn.edu.ec>
2024-11-20 06:24:28 -0500

    siii

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,6 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Cálculo de Altura Máxima y Tiempo de Vuelo para un Movimiento Parabólico");
-
-        System.out.print("Ingrese la velocidad inicial (m/s): ");
-        double velocidadInicial = scanner.nextDouble();
-
-        System.out.print("Ingrese el ángulo de lanzamiento (grados): ");
-        double angulo = scanner.nextDouble();
-
-        System.out.print("Ingrese la aceleración por gravedad (m/s^2): ");
-        double gravedad = scanner.nextDouble();
-
-        Proyectil proyectil = new Proyectil();
-        proyectil.mostrarResultados();
-        System.out.println(" ");
-        proyectil.setAngulo(30);
-        proyectil.setGravedad(26.2);
-        proyectil.setVelocidadInicial(150);
-        proyectil.mostrarResultados();
-
     }
 }
 
